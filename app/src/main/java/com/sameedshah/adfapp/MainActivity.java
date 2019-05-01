8e0d01fea8a2bbe08290bc4ea5eb4e19bfa4756c
Sameed Shah <sameedshah9@gmail.com>
2019-05-02 00:03:13 +0500

    Fragment added in admin notes section

--- app/src/main/java/com/sameedshah/adfapp/MainActivity.java
+++ app/src/main/java/com/sameedshah/adfapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sameedshah.adfapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView adminNotes = findViewById(R.id.btnAdminNotes);
+        adminNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AdminNotes.class));
+            }
+        });
 
     }
 }
