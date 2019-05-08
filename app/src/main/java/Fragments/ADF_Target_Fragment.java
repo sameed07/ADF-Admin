5006b4fb96da5ce95ea71d9c90648aae4f9b0210
Sameed Shah <sameedshah9@gmail.com>
2019-05-08 23:48:52 +0500

    some fixing in fragments

--- app/src/main/java/Fragments/ADF_Target_Fragment.java
+++ app/src/main/java/Fragments/ADF_Target_Fragment.java
@@ -5,17 +5,67 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.sameedshah.adfapp.R;
 
 public class ADF_Target_Fragment extends Fragment {
+
+    EditText edt_target,edt_done,edt_lessValue,edt_vsTarget,edt_lastYear,edt_trend,aramis_target;
+    ImageView add_brand;
+    FirebaseDatabase mDatabase;
+    DatabaseReference mRef;
+    Button btnSave;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.adf_target_fragment, container, false);
 
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference("ADFTarget");
+
+        edt_target = view.findViewById(R.id.edt_target);
+        edt_done = view.findViewById(R.id.edt_done);
+        edt_lessValue = view.findViewById(R.id.edt_lessValue);
+        edt_vsTarget = view.findViewById(R.id.edt_vsTarget);
+        edt_lastYear = view.findViewById(R.id.edt_lastYear);
+        edt_trend = view.findViewById(R.id.edt_trend);
+        add_brand = view.findViewById(R.id.add_brand);
+
+        btnSave = view.findViewById(R.id.onSave);
+
+        add_brand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "asdf", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
         return view;
     }
+
+    public void dataSaved(){
+
+    }
+
 }
--- app/src/main/java/Fragments/FYTD_Fragment.java
+++ app/src/main/java/Fragments/FYTD_Fragment.java
@@ -5,45 +5,56 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.sameedshah.adfapp.R;
 
 public class FYTD_Fragment extends Fragment {
 
-    EditText edt_target,edt_done,edt_lessValue,edt_vsTarget,edt_lastYear,edt_trend,aramis_target;
-    EditText aramis_trend,aramis_mtd,lessValue;
-    EditText dkny_target,dkny_trend,dkny_mtd,dkny_lessValue;
-    EditText mk_target, mk_trend, mk_mtd, mk_lessValue;
-    EditText zegna_target, zegna_trend,zegma_mtd,zegna_lessValue;
+
+//    EditText aramis_trend,aramis_mtd,lessValue;
+//    EditText dkny_target,dkny_trend,dkny_mtd,dkny_lessValue;
+//    EditText mk_target, mk_trend, mk_mtd, mk_lessValue;
+//    EditText zegna_target, zegna_trend,zegma_mtd,zegna_lessValue;
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fytd_fragment, container, false);
 
-        edt_target = view.findViewById(R.id.edt_target);
-        edt_done = view.findViewById(R.id.edt_done);
-        edt_lessValue = view.findViewById(R.id.edt_lessValue);
-        edt_vsTarget = view.findViewById(R.id.edt_vsTarget);
-        edt_lastYear = view.findViewById(R.id.edt_lastYear);
-        edt_trend = view.findViewById(R.id.edt_trend);
 
-        aramis_target = view.findViewById(R.id.aramis_target);
-        aramis_trend = view.findViewById(R.id.aramis_trend);
-        aramis_mtd = view.findViewById(R.id.aramis_mtd);
-        lessValue = view.findViewById(R.id.aramis_target);
-
-        dkny_target = view.findViewById(R.id.dkny_target);
-        dkny_trend = view.findViewById(R.id.dkny_trend);
-        dkny_mtd = view.findViewById(R.id.dkny_mtd);
-        dkny_lessValue = view.findViewById(R.id.dkny_lessValue);
-
-        mk_target = view.findViewById(R.id.mk_target);
-        mk_trend = view.findViewById(R.id.mk_trend);
-        mk_mtd = view.findViewById(R.id.mk_mtd);
-        mk_lessValue = view.findViewById(R.id.mk_lessValue) ;
+//
+//        aramis_target = view.findViewById(R.id.aramis_target);
+//        aramis_trend = view.findViewById(R.id.aramis_trend);
+//        aramis_mtd = view.findViewById(R.id.aramis_mtd);
+//        lessValue = view.findViewById(R.id.aramis_target);
+//
+//        dkny_target = view.findViewById(R.id.dkny_target);
+//        dkny_trend = view.findViewById(R.id.dkny_trend);
+//        dkny_mtd = view.findViewById(R.id.dkny_mtd);
+//        dkny_lessValue = view.findViewById(R.id.dkny_lessValue);
+//
+//        mk_target = view.findViewById(R.id.mk_target);
+//        mk_trend = view.findViewById(R.id.mk_trend);
+//        mk_mtd = view.findViewById(R.id.mk_mtd);
+//        mk_lessValue = view.findViewById(R.id.mk_lessValue) ;
         return view;
     }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+    }
+
+
 }
