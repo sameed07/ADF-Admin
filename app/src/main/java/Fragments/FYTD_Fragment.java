d71aec99df4cce5703e1a44dc6286f0fd77898c1
Sameed Shah <sameedshah9@gmail.com>
2019-05-13 11:51:13 +0500

    fragements updated

--- app/src/main/java/Fragments/FYTD_Fragment.java
+++ app/src/main/java/Fragments/FYTD_Fragment.java
@@ -1,9 +1,11 @@
 package Fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,13 +13,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,7 +32,10 @@ import com.google.firebase.database.ValueEventListener;
 import com.sameedshah.adfapp.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import Adapters.BrandAdapter;
 import Model.Brands;
 import Model.TargetModel;
 
@@ -54,10 +63,10 @@ public class FYTD_Fragment extends Fragment {
 
 
         mDatabase = FirebaseDatabase.getInstance();
-        mRef = mDatabase.getReference("Targets").child("Quarterly");
+        mRef = mDatabase.getReference("Targets").child("FYTD");
 
         FirebaseDatabase mdatabase = FirebaseDatabase.getInstance();
-        brandRef = mdatabase.getReference("Brands").child("ADF");
+        brandRef = mdatabase.getReference("Brands").child("FYTD");
 
         edt_target = view.findViewById(R.id.edt_target);
         edt_done = view.findViewById(R.id.edt_done);
@@ -70,11 +79,15 @@ public class FYTD_Fragment extends Fragment {
 
 
 
+        brandsRecycler = view.findViewById(R.id.brandsRecycler);
+        layoutManager = new LinearLayoutManager(getContext());
+        brandsRecycler.setLayoutManager(layoutManager);
+        brandsRecycler.setHasFixedSize(true);
 
         add_brand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                showBrandDialog();
 
             }
         });
@@ -110,6 +123,7 @@ public class FYTD_Fragment extends Fragment {
                         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
                             dataSaved(mList.get(position));
+                            showBrands(mList.get(position));
 
                         }
 
@@ -155,4 +169,100 @@ public class FYTD_Fragment extends Fragment {
             }
         });
     }
+
+
+    public void showBrandDialog(){
+
+
+
+        final Dialog mdialog = new Dialog(getContext(),android.R.style.Theme_DeviceDefault_NoActionBar_Fullscreen);
+        mdialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        mdialog.setContentView(R.layout.brands_dialog);
+        mdialog.show();
+        //   EditText edt_location = mdialog.findViewById(R.id.edtLocation);
+        final EditText edt_brand = mdialog.findViewById(R.id.edtBrandName);
+        final EditText edt_target = mdialog.findViewById(R.id.edtTarget);
+        final EditText edt_mdt = mdialog.findViewById(R.id.edt_mtd);
+        final EditText edt_trend = mdialog.findViewById(R.id.edt_trend);
+        final EditText edt_less = mdialog.findViewById(R.id.edt_lessValue);
+        final Spinner locaionSpin = mdialog.findViewById(R.id.locationSpinner);
+        Button btnSave = mdialog.findViewById(R.id.btnSave);
+        ArrayAdapter adapter = new ArrayAdapter(getContext(),android.R.layout.simple_spinner_dropdown_item,mList);
+        locaionSpin.setAdapter(adapter);
+        loadData();
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if( !edt_brand.getText().toString().equals("") &&
+                        !edt_target.getText().toString().equals("") && !edt_mdt.getText().toString().equals("") &&
+                        !edt_trend.getText().toString().equals("") && !edt_less.getText().toString().equals("")) {
+
+                    Map<String,Object> map = new HashMap<>();
+                    map.put("location", locaionSpin.getSelectedItem().toString());
+                    map.put("brand_name", edt_brand.getText().toString());
+                    map.put("target",edt_target.getText().toString());
+                    map.put("mdt", edt_mdt.getText().toString());
+                    map.put("trend", edt_trend.getText().toString());
+                    map.put("less_value",edt_less.getText().toString());
+
+                    brandRef = brandRef.child(locaionSpin.getSelectedItem().toString());
+
+                    brandRef.push().setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()){
+                                Toast.makeText(getContext(), "Data Added", Toast.LENGTH_SHORT).show();
+                                getActivity().finish();
+                            }else{
+                                Toast.makeText(getContext(), "Err :" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+                }else{
+                    Toast.makeText(getContext(), "All fields must not be empty!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+
+    public void showBrands(String location){
+
+        final BrandAdapter adapter = new BrandAdapter(getContext(),brandsList);
+        brandRef.child(location).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                if(dataSnapshot.exists()){
+                    brandsList.clear();
+                    for(DataSnapshot ds : dataSnapshot.getChildren()){
+                        Brands brands = ds.getValue(Brands.class);
+                        brandsList.add(brands);
+                    }
+
+                    brandsRecycler.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }else{
+                    brandsList.clear();
+                    adapter.notifyDataSetChanged();
+                    Toast.makeText(getContext(), "Data does not exists", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/Fragments/Quarterly_Fragment.java
+++ app/src/main/java/Fragments/Quarterly_Fragment.java
@@ -1,5 +1,6 @@
 package Fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,13 +10,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,7 +29,10 @@ import com.google.firebase.database.ValueEventListener;
 import com.sameedshah.adfapp.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import Adapters.BrandAdapter;
 import Model.Brands;
 import Model.TargetModel;
 
@@ -53,7 +61,7 @@ public class Quarterly_Fragment  extends Fragment {
         mRef = mDatabase.getReference("Targets").child("Quarterly");
 
         FirebaseDatabase mdatabase = FirebaseDatabase.getInstance();
-        brandRef = mdatabase.getReference("Brands").child("ADF");
+        brandRef = mdatabase.getReference("Brands").child("Quarterly");
 
         edt_target = view.findViewById(R.id.edt_target);
         edt_done = view.findViewById(R.id.edt_done);
@@ -64,10 +72,10 @@ public class Quarterly_Fragment  extends Fragment {
         add_brand = view.findViewById(R.id.add_brand);
         locationSpinner = view.findViewById(R.id.locationSpinner);
 
-     //   brandsRecycler = view.findViewById(R.id.brandsRecycler);
-        ///layoutManager = new LinearLayoutManager(getContext());
-       // brandsRecycler.setLayoutManager(layoutManager);
-       // brandsRecycler.setHasFixedSize(true);
+        brandsRecycler = view.findViewById(R.id.brandsRecycler);
+        layoutManager = new LinearLayoutManager(getContext());
+        brandsRecycler.setLayoutManager(layoutManager);
+        brandsRecycler.setHasFixedSize(true);
 
 
 
@@ -75,6 +83,7 @@ public class Quarterly_Fragment  extends Fragment {
             @Override
             public void onClick(View v) {
 
+                showBrandDialog();
 
             }
         });
@@ -110,6 +119,7 @@ public class Quarterly_Fragment  extends Fragment {
                         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
                             dataSaved(mList.get(position));
+                            showBrands(mList.get(position));
 
                         }
 
@@ -147,6 +157,100 @@ public class Quarterly_Fragment  extends Fragment {
                     edt_vsTarget.setText(tm.getVs_target());
 
                 }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void showBrandDialog(){
+
+
+
+        final Dialog mdialog = new Dialog(getContext(),android.R.style.Theme_DeviceDefault_NoActionBar_Fullscreen);
+        mdialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        mdialog.setContentView(R.layout.brands_dialog);
+        mdialog.show();
+        //   EditText edt_location = mdialog.findViewById(R.id.edtLocation);
+        final EditText edt_brand = mdialog.findViewById(R.id.edtBrandName);
+        final EditText edt_target = mdialog.findViewById(R.id.edtTarget);
+        final EditText edt_mdt = mdialog.findViewById(R.id.edt_mtd);
+        final EditText edt_trend = mdialog.findViewById(R.id.edt_trend);
+        final EditText edt_less = mdialog.findViewById(R.id.edt_lessValue);
+        final Spinner locaionSpin = mdialog.findViewById(R.id.locationSpinner);
+        Button btnSave = mdialog.findViewById(R.id.btnSave);
+        ArrayAdapter adapter = new ArrayAdapter(getContext(),android.R.layout.simple_spinner_dropdown_item,mList);
+        locaionSpin.setAdapter(adapter);
+        loadData();
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if( !edt_brand.getText().toString().equals("") &&
+                        !edt_target.getText().toString().equals("") && !edt_mdt.getText().toString().equals("") &&
+                        !edt_trend.getText().toString().equals("") && !edt_less.getText().toString().equals("")) {
+
+                    Map<String,Object> map = new HashMap<>();
+                    map.put("location", locaionSpin.getSelectedItem().toString());
+                    map.put("brand_name", edt_brand.getText().toString());
+                    map.put("target",edt_target.getText().toString());
+                    map.put("mdt", edt_mdt.getText().toString());
+                    map.put("trend", edt_trend.getText().toString());
+                    map.put("less_value",edt_less.getText().toString());
+
+                    brandRef = brandRef.child(locaionSpin.getSelectedItem().toString());
+
+                    brandRef.push().setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()){
+                                Toast.makeText(getContext(), "Data Added", Toast.LENGTH_SHORT).show();
+                                getActivity().finish();
+                            }else{
+                                Toast.makeText(getContext(), "Err :" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+                }else{
+                    Toast.makeText(getContext(), "All fields must not be empty!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+
+    public void showBrands(String location){
+
+        final BrandAdapter adapter = new BrandAdapter(getContext(),brandsList);
+        brandRef.child(location).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                if(dataSnapshot.exists()){
+                    brandsList.clear();
+                    for(DataSnapshot ds : dataSnapshot.getChildren()){
+                        Brands brands = ds.getValue(Brands.class);
+                        brandsList.add(brands);
+                    }
+
+                    brandsRecycler.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }else{
+                    brandsList.clear();
+                    adapter.notifyDataSetChanged();
+                    Toast.makeText(getContext(), "Data does not exists", Toast.LENGTH_SHORT).show();
+                }
+
             }
 
             @Override
